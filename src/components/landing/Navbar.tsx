1d4f50bba21ec28ee74bfc71709a58497cb4a50c
tanawatchaijaroenice <153838512+tanawatchaijaroenice@users.noreply.github.com>
2025-06-06 22:49:16 +0700

    fix logo width, nevbar when scroll

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -1,13 +1,29 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > window.innerHeight) {
+                setIsScrolled(true)
+            } else {
+                setIsScrolled(false)
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [])
+
     return (
-        <header className="fixed top-0 z-50 p-4 backdrop-blur-sm w-full">
+        <header className={`fixed top-0 z-50 p-4 w-full transition-all duration-300 ${isScrolled ? 'backdrop-blur-md bg-black/20' : ''
+            }`}>
             <nav className="flex justify-between items-center">
                 <Link href="/">
-                    <Image src='/images/Baralee_logo_White2.svg' alt="Logo" width={1200} height={1200} className="cursor-pointer w-[150px]" />
+                    <Image src='/images/Baralee_logo_White2.svg' alt="Logo" width={3000} height={3000} className="cursor-pointer w-[150px]" />
                 </Link>
 
                 <span className="text-lg text-white">tel: (+66) 97 564 2435</span>
